d9ea5325f34335a79cda1915dc3ea15cff131a2b
tam <tam@gmail.com>
2022-11-26 22:23:45 +0700

    a

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-export DATABASE_URL="postgres://ujldzdumwmqgmo:462b05e6c2d3828cd40994886ffd528ec9398fa0dcea28ca2e10fd00332a86f2@ec2-52-204-157-26.compute-1.amazonaws.com:5432/d8k1e0r5m2jnd7"
+ export DATABASE_URL="postgres://ujldzdumwmqgmo:462b05e6c2d3828cd40994886ffd528ec9398fa0dcea28ca2e10fd00332a86f2@ec2-52-204-157-26.compute-1.amazonaws.com:5432/d8k1e0r5m2jnd7"
 export EXCITED="true"
+# export DATABASE_URL="postgresql://postgres:admin@localhost:5433/postgres"
 echo "setup.sh script executed successfully!"
